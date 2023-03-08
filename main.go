a42f6fee8f0ce7d6a5057729c464450f51ceeef1
mohammed.adekunle <adekunle3317@gmail.com>
2023-03-08 20:32:37 +0100

    Setup initial app state

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"github.com/jackc/pgx/v5"
+	"github.com/redis/go-redis/v9"
+	"github.com/the-code-genin/busha-test/internal"
+)
+
+var pgConn *pgx.Conn
+var redisClient *redis.Client
+
+func init() {
+	// Load env variables into memory
+	err := internal.LoadEnvVariables()
+	if err != nil {
+		panic(err)
+	}
+
+	// Connect to postgres db
+	pgConn, err = internal.ConnectToPostgres()
+	if err != nil {
+		panic(err)
+	}
+
+	// Connect to redis
+	redisClient, err = internal.ConnectToRedis()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	// init
+	// Main
 }
